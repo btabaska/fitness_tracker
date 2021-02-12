5ae325edfff21728e48c0b0bb9f4acb0bc467a4a
Brandon Tabaska <btabaska@gmail.com>
2021-02-11 17:36:50 -0800

    added first route

--- routes/api.js
+++ routes/api.js
@@ -12,9 +12,22 @@ router.get("/api/workouts", (req, res) => {
       res.status(400).json(err);
     });
 });
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
 
 router.get("/exercise", function (req, res) {
   res.sendFile(path.join(__dirname, "..", "public", "exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "..", "public", "stats.html"));
 });
 
 module.exports = router;
